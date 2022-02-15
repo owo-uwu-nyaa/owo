275c822c5548ee5e69a2dacf0a89d951c12c168d
langj <langj@cs.tum.edu>
2022-02-15 20:09:27 +0100

    rewrite main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #! /bin/python3
+import asyncio
 import csv
 import datetime
 import random
@@ -6,33 +7,54 @@ import subprocess
 import sys
 import threading
 import time
+from multiprocessing import Process
+
 import discord
 import requests
+from discord.ext import commands
+
 import owo
 import uwu_data
 
 react_on = "$"
 client = discord.Client()
 csv_writer_lock = threading.Lock()
-csvfile = open('/home/langj/owo/msgs.csv', 'a', newline='')
+q_writer_lock = threading.Lock()
+bpath = sys.argv[1]
+csvfile = open(bpath + 'msgs.csv', 'a', newline='')
 msgwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-gen_res = open('/home/langj/owo/generated9.owo')
-gen_reader = csv.reader(gen_res)
+
+qcl = []
+qci = []
+qxi = 0
+qrfile = open(bpath + 'quotes.csv', 'r', newline='')
+qtreader = csv.reader(qrfile)
+for qt in qtreader:
+    qcl.append(qt[0])
+    qci.append(qt[1])
+    qxi += 1
+
+qfile = open(bpath + 'quotes.csv', 'a', newline='')
+qwriter = csv.writer(qfile, quoting=csv.QUOTE_MINIMAL)
+gen_res = open(bpath + 'stripped_msgs.txt')
+gens = gen_res.read().split("====================")
+
 
 BASE = "https://discord.com/api/v9/"
-TOKEN = "Nzc5NDEwMDM4NDQ3NDA3MTM2.X7gIKQ.2yMGGTAg-0BBQ_vTj4Jh4TdvrxQ"
-client.run(TOKEN)
-bot = discord.Bot()
-bot.run(TOKEN)
+TOKEN = open(bpath + "token.owo", "r").read()
+desc = "Pwease end my misewy, nyaaa~"
+bot = commands.Bot(command_prefix=react_on, description=desc)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
+
 
 @client.event
 async def on_ready():
     print(f"Hellowo, mwy nwame is {client.user}")
 
-
-@bot.slash_command()
-async def hello(ctx):
-    await ctx.respond("Hewwo:)")
 
 def timeout_user(*, user_id: int, guild_id: int, until: int):
     endpoint = f'guilds/{guild_id}/members/{user_id}'
@@ -46,70 +68,114 @@ def timeout_user(*, user_id: int, guild_id: int, until: int):
     else:
         return print("Did not find any\n", session.status_code)
 
+
+@bot.command()
+async def qt(ctx, subcommand: str, content: str):
+    print("hellowo")
+    if subcommand == "add":
+        qcl.append(content)
+        qci.append(len(qcl) - 1)
+        with q_writer_lock:
+            qwriter.writerow([len(qcl) - 1, content])
+            qfile.flush()
+    else:
+        if len(content) > 0:
+            content = f" {content}"
+        qs = subcommand + content
+        qtf = None
+        if qs == "":
+            qtf = random.choice(qcl)
+        else:
+            qcl_match = list(filter(lambda x: qs[1:] in x, qcl))
+            if len(qcl_match) > 0:
+                qtf = random.choice(qcl_match)
+            else:
+                qtf = random.choice(qcl)
+        await ctx.send(f'```{qtf.replace("`", "")} ```')
+
+def genRow(maxlen: int, curRow: int):
+    ltr = chr(ord('A') + curRow)
+    opad = maxlen - curRow - 1
+    return f"{'.' * opad}{ltr}{'.' * curRow * 2}{ltr}{'.' * opad}"
+
+@bot.command()
+async def diamond(ctx, nrows: int):
+    if nrows > 22:
+        await ctx.send(f"{random.choice(uwu_data.sowwy)}, thiws is too mwuch fow me to take nyaaa~")
+        return
+    else:
+        result = []
+        for i in range(0, nrows - 1):
+            result.append(genRow(nrows, i))
+        for i in range(nrows - 1, -1, -1):
+            result.append(genRow(nrows, i))
+        d = '\n'.join(result)
+        await ctx.send(f"```\n{d}\n```")
+
+@bot.command()
+async def obamamedal(ctx):
+    await ctx.send("https://media.discordapp.net/attachments/798609300955594782/816701722818117692/obama.jpg")
+
+@bot.command()
+async def owobamamedal(ctx):
+    await ctx.send("https://cdn.discordapp.com/attachments/938102328282722345/939605208999264367/Unbenannt.png")
+
+@bot.command()
+async def crash():
+    sys.exit(0)
+
+@bot.command()
+async def hello(ctx):
+    ctx.send(random.choice(["Hello", "Hello handsome :)"]))
+
+@bot.command()
+async def generate(ctx):
+    resp = gens.pop()
+    ctx.send(resp.replace("everyone", "evwyone").replace("here","hewe"))
+
+@bot.command()
+async def bottom(ctx, msg: str):
+    bottom = subprocess.run([bpath + "bottomify", "-b", msg], capture_output=True)
+    await ctx.send(bottom.stdout.decode("utf8"))
+
+@bot.command()
+async def unbottom(ctx, msg: str):
+    uwu = subprocess.run([bpath + "bottomify", "-r", msg], capture_output=True)
+    await ctx.send(uwu.stdout.decode("utf8"))
+
+@bot.command()
+async def love(ctx, msg: str):
+    user = ctx.message.mentions[0]
+    for i in range(0, random.randint(5, 25)):
+        time.sleep(1)
+        await user.send('👀')
+
+@bot.command(brief="OwO")
+async def owo(ctx):
+    await ctx.send(random.choice(uwu_data.int_emote))
+
+@bot.command(brief="owofy <msg> nyaa~~")
+async def owofy(ctx, msg: str):
+    owofied = owo.owofy(msg)
+    await ctx.send(f'```{owofied.replace("`", "")} ```')
+
+@bot.command(brief="telwlws you how owo-kawai <msg> is - scowre >= 1 is owo :3")
+async def rate(ctx, msg: str):
+    score = owo.score(msg)
+    await ctx.send(f'S-Senpai ywou scwored a {score:.2f}')
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    elif message.author.id == -1:
+    if message.author.id == -1:
         print("trying to timeout uwu")
         await timeout_user(user_id=message.author.id, guild_id=message.guild.id, until=1)
-    if message.content == (f"{react_on}generate"):
-        resp = next(gen_reader)[0][:1999].replace("<|startoftext|>","\n").replace("<|endoftext|>","").replace("<","")
-        await message.channel.send(resp)
-    if message.content == f"{react_on}obamamedal":
-        await message.channel.send("https://media.discordapp.net/attachments/798609300955594782/816701722818117692/obama.jpg")
-    if message.content == f"{react_on}owobamamedal":
-        await message.channel.send("https://cdn.discordapp.com/attachments/938102328282722345/939605208999264367/Unbenannt.png")
-    if message.content == f"{react_on}crash":
-        sys.exit(0)
-    if message.content == f"{react_on}hello":
-        rep = "Hello"
-        if random.randint(0, 10) > 5:
-            rep += " handsome!"
-        else:
-            rep += "!"
-        await message.channel.send(rep)
-    elif message.content.startswith(f"{react_on}bottom"):
-        bottomify = message.content.split(f'{react_on}bottom', 1)[1]
-        uwu = subprocess.run(["home/langj/nowobot/bottomify", "-b", bottomify], capture_output=True)
-        await message.channel.send(uwu.stdout.decode("utf8"))
-    elif message.content.startswith(f"{react_on}unbottom"):
-        bottomify = message.content.split(f'{react_on}unbottom', 1)[1]
-        print(bottomify)
-        uwu = subprocess.run(["./bottomify", "-r", bottomify], capture_output=True)
-        await message.channel.send(uwu.stdout.decode("utf8"))
-    elif message.content == f"{react_on}help":
-        rep = """```K-Konichiwa!
-$help - showws thwis mwessage 
-$rate <msg> - telwlws you how owo-kawai <msg> is - scowre >= 1 is owo :3
-$owofy <msg> - owofy <msg> nyaa~~
-$owo - OwO```"""
-        await message.channel.send(rep)
-    elif message.content.startswith(f"{react_on}love"):
-        user = message.mentions[0]
-        for i in range(0, random.randint(5, 25)):
-            time.sleep(1)
-            await user.send('👀')
-    elif message.content == f"{react_on}pengu":
-        t = ""
-        while len(t) < 10:
-            normal = subprocess.run(["./random_pengu.sh", ""], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                    check=True, text=True)
-            t = normal.stdout
-        await message.channel.send(t)
-    elif message.content == f"{react_on}owo":
-        await message.channel.send(random.choice(uwu_data.int_emote))
-    elif message.content.startswith(f"{react_on}owofy "):
-        answer = f"{owo.owofy(message.content.split(f'{react_on}owofy', 1)[1])}"
-        await message.channel.send(f'```{answer.replace("`", "")} ```')
-    elif message.content.startswith(f"{react_on}rate "):
-        score = owo.score(message.content.split(f'{react_on}rate', 1)[1])
-        await message.channel.send(f'S-Senpai ywou scwored a {score:.2f}')
-    else:
+    if message.channel.id == 937306121901850684:
         words = message.content.split()
         nmsg = ""
         for word in words:
-            if not word.startswith("http") and not(word[0] == "<" and word[-1] == ">"):
+            if not word.startswith("http") and not (word[0] == "<" and word[-1] == ">"):
                 nmsg += f"{word} "
         msg = nmsg
         owo_score = owo.score(msg)
@@ -118,24 +184,17 @@ $owo - OwO```"""
         for ltr in msg:
             if ltr.isalpha():
                 is_only_alpha = False
-        if owo_score < 1 and not is_only_alpha and message.channel.id == 937306121901850684:
+        if owo_score < 1 and not is_only_alpha:
             answer = owo.owofy(msg)
-            spoiler_tok = answer.split('||')
-            na = ""
-            is_spoiler = False
-            for tok in range(0, len(spoiler_tok)):
-                is_spoiler = not is_spoiler
-                if is_spoiler and tok + 1 < len(spoiler_tok):
-                    na += "█" * len(spoiler_tok[tok])
-                else:
-                    na += spoiler_tok[tok]
-            answer = na
             answer_score = owo.score(answer)
             print(answer_score)
             await message.channel.send(
-                    f'{random.choice(uwu_data.sowwy)} <@{message.author.id}> youw seem to nwot hav owofied ywour text.. h-here lwet me show you:\n```{answer.replace("`", "")} ```')
-    print(message.author.id)
+                f'{random.choice(uwu_data.sowwy)} <@{message.author.id}> youw seem to nwot hav owofied ywour text.. h-here lwet me show you:\n```{answer.replace("`", "")} ```')
     with csv_writer_lock:
         msgwriter.writerow([message.author.id, message.channel.id, time.time(), message.content])
-        csvfile.flush()
+    csvfile.flush()
 
+loop = asyncio.get_event_loop()
+loop.create_task(bot.start(TOKEN))
+loop.create_task(client.start(TOKEN))
+loop.run_forever()
