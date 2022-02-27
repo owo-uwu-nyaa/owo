1ecc9c9f09bfc76371b8250896c5b5321b601bf7
Jonas <44544539+likeazir@users.noreply.github.com>
2022-02-27 22:54:44 +0100

    Unit 00 (#4)
    * commit a draft version of the history feature
    
    * change savepath for history
    
    * high res img
    
    * dont write to actual fiel
    
    * add lock to history
    
    * fix formatting
    
    * fix ide formatting
    
    * fill missing values with zeros in timeseries
    
    Co-authored-by: langj <langj@xmm.fritz.box>
    Co-authored-by: Jonas Lang <ge25top@tum.de>

--- src/common.py
+++ src/common.py
@@ -5,3 +5,9 @@ import discord
 def get_nick_or_name(user: object) -> str:
     gnick = user.nick if isinstance(user, discord.Member) and not user.nick is None else user.name
     return re.sub(r'[@!$`]', "", gnick)
+
+async def author_id_to_obj(bot, author_id, ctx):
+    author = ctx.guild.get_member(author_id)
+    if author is None:
+        author = await bot.fetch_user(author_id)
+    return author
--- src/stats.py
+++ src/stats.py
@@ -1,4 +1,10 @@
 import threading
+import io
+import discord
+import pandas
+
+import common
+import plotly.express as px
 
 from discord.ext import commands
 from pyspark.shell import spark
@@ -17,7 +23,6 @@ class Stats(commands.Cog):
     def __init__(self, bot, bpath):
         self.bot = bot
         self.spark_lock = threading.Lock()
-        sc = spark.sparkContext
 
         schema = StructType([
             StructField("author_id", LongType(), False),
@@ -86,3 +91,36 @@ class Stats(commands.Cog):
                 .orderBy("count", ascending=False)
             res = get_show_string(dfl, n=20)
             await ctx.channel.send(f'```\n{res.replace("`", "")}\n```')
+
+    @stats.command(brief="make history")
+    async def history(self, ctx, *members: discord.Member):
+        if not await self.check_allow_query(ctx):
+            return
+        with self.spark_lock:
+            author_mappings = []
+            if len(members) == 0:
+                dfq = self.df.groupBy("author_id").count().orderBy("count", ascending=False).limit(10)
+                for r in dfq.collect():
+                    author_mappings.append((common.get_nick_or_name(
+                        await common.author_id_to_obj(self.bot, r["author_id"], ctx)), r["author_id"]))
+            else:
+                for m in members:
+                    author_mappings.append((common.get_nick_or_name(m), m.id))
+            author_mappings_df = spark.createDataFrame(data=author_mappings, schema=["name", "id"])
+            dft = self.df.drop("channel_id", "msg") \
+                .join(author_mappings_df.drop("name"), self.df["author_id"] == author_mappings_df["id"]) \
+                .withColumn("date", date_trunc("day", "u_time")) \
+                .groupBy("date", "author_id") \
+                .agg(count("date").alias("count"))
+            dfp = dft.toPandas() \
+                .pivot(index="date", columns="author_id", values="count") \
+                .asfreq("1D", fill_value=0) \
+                .fillna(0) \
+                .reset_index() \
+                .melt(id_vars=["date"]) \
+                .join(author_mappings_df.toPandas().set_index("id"), on="author_id")
+            fig = px.line(dfp, x="date", y="value", color="name")
+            img = io.BytesIO()
+            fig.write_image(img, format="png", scale=3)
+            img.seek(0)
+            await ctx.channel.send(file=discord.File(fp=img, filename="yeet.png"))
