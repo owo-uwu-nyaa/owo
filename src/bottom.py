9550c9b732ce3eaf04b2f90274b122b391b542ac
Robin Marchart <robin.marchart@tum.de>
2022-02-28 23:27:47 +0100

    fix bottom commands (#7)
    use asyncio subprocess to not block event loop and prevetn interpretation of input as commandline argument

--- src/bottom.py
+++ src/bottom.py
@@ -1,19 +1,34 @@
-import subprocess
+import asyncio.subprocess as sub
 from discord.ext import commands
 
 
 class Bottom(commands.Cog):
 
-    def __init__(self, bot, bpath):
-        self.bpath = bpath
+    def __init__(self, bot, bpath:str):
+        self.bottom_cmd = bpath+"bottomify"
         self.bot = bot
+
+    async def call_bottom(self,arg:str,msg:str):
+        if msg.startswith("-"):
+            #prepend zero width space to prevent interpretation as command line argument
+            msg="\ue2808b"+msg
+        process=await sub.create_subprocess_exec(
+            self.bottom_cmd,
+            arg,
+            msg,
+            stdin=sub.PIPE,
+            stdout=sub.PIPE,
+            stderr=sub.DEVNULL
+        )
+        (uwu,_)=await process.communicate()
+        return uwu.decode("utf-8")
 
     @commands.command()
     async def bottom(self, ctx, *, msg: str):
-        bottom = subprocess.run([self.bpath + "bottomify", "-b", msg], capture_output=True)
-        await ctx.send(bottom.stdout.decode("utf8"))
+        bottom = await self.call_bottom("-b",msg)
+        await ctx.send(bottom)
 
     @commands.command()
     async def unbottom(self, ctx, *, msg: str):
-        uwu = subprocess.run([self.bpath + "bottomify", "-r", msg], capture_output=True)
-        await ctx.send(uwu.stdout.decode("utf8"))
+        uwu = await self.call_bottom("-r",msg)
+        await ctx.send(uwu)
