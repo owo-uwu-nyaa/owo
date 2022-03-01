e8f2ee2213c52d00068a799c6ffbd4f9d585b6fc
Robin Marchart <robin.marchart@tum.de>
2022-03-01 17:52:46 +0100

    implement centralized config
    owo

---
+++ src/config.py
@@ -0,0 +1,54 @@
+from os import read
+import os.path as path
+import toml
+
+class MissingKeyException(Exception):
+    def __init__(self, *keys:str) -> None:
+        super().__init__(*keys)
+
+def get_key(user,default,*path):
+    if user==None and default==None:
+        raise MissingKeyException(*path)
+    elif len(path)==0:
+        return user if user!=None else default
+    else:
+        key=path[1]
+        try:
+            return get_key(
+                user[key] if user!=None and key in user else None,
+                default[key] if default!=None and key in default else None,
+                *(path[1:])
+            )
+        except MissingKeyException:
+            raise MissingKeyException(*path)
+
+class Config:
+
+    def __init__(self,config_file:str):
+        config=None
+        try:
+            config=toml.load(open(config_file,mode="r"))
+        except OSError as e:
+            print(f"unable to open config file {config_file}: {e}")
+        except (TypeError,toml.TomlDecodeError) as e:
+            print(f"invalid config file: {e}")
+        src_dir,_=path.split(path.realpath(__file__))
+        default_config_file=path.join(src_dir,"../owo.toml")
+        default_config=None
+        if not path.samefile(config_file,default_config_file):
+            try:
+                default_config=toml.load(open(default_config_file,mode="r"))
+            except OSError as e:
+                print(f"unable to open default config file {default_config_file}: {e}")
+            except (TypeError,toml.TomlDecodeError) as e:
+                print(f"invalid default config file: {e}")
+
+        self.command_prefix=str(get_key(config,default_config,"command_prefix"))
+        self.desc=str(get_key(config,default_config,"desc"))
+
+        self.message_file=str(get_key(config,default_config,"messages","file"))
+
+        self.bottom_cmd=str(get_key(config,default_config,"bottom","executable"))
+
+        self.catapi_token=str(get_key(config,default_config,"api_tokens","catapi"))
+        self.discord_token=str(get_key(config,default_config,"api_tokens","discord"))
--- src/main.py
+++ src/main.py
@@ -11,28 +11,29 @@ from msg_writer import MsgWriter
 from simple_commands import Misc
 from owo import Owo
 from stats import Stats
+from config import Config
 
 
 def main():
-    react_on = "$"
-    bpath = f"{sys.argv[1]}/"
-    token = open(bpath + "token.owo", "r").read()
-    desc = "Pwease end my misewy, nyaaa~"
+    config_file="owo.toml"
+    if len(sys.argv)>1:
+        config_file=sys.argv[1]
+    config=Config(config_file)
     intents = discord.Intents().all()
     allowed_mentions = discord.AllowedMentions(users=True, everyone=False, roles=False, replied_user=False)
-    bot = commands.Bot(command_prefix=react_on, description=desc, intents=intents, allowed_mentions=allowed_mentions)
+    bot = commands.Bot(command_prefix=config.command_prefix, description=config.desc, intents=intents, allowed_mentions=allowed_mentions)
 
     bot.add_cog(Hugs(bot))
-    bot.add_cog(MsgWriter(bot, bpath))
-    bot.add_cog(Aww(bot, bpath))
+    bot.add_cog(MsgWriter(bot, config.message_file))
+    bot.add_cog(Aww(bot, config.catapi_token))
     bot.add_cog(Misc(bot))
     bot.add_cog(Owo(bot))
-    bot.add_cog(Admin(bot, bpath))
-    bot.add_cog(Stats(bot, bpath))
-    bot.add_cog(Bottom(bot, bpath))
+    bot.add_cog(Admin(bot))
+    bot.add_cog(Stats(bot, config.message_file))
+    bot.add_cog(Bottom(bot, config.bottom_cmd))
     # bot.add_cog(Quotes(bot))
     loop = asyncio.get_event_loop()
-    loop.create_task(bot.start(token))
+    loop.create_task(bot.start(config.discord_token))
     loop.run_forever()
 
 
