082aa81dc22f990a62ec58b1658ecbb208a79672
Simon Huang <simon.huang@tum.de>
2022-03-26 20:51:20 +0100

    add dynamic import and instantiation of cogs

--- owobot/uwubot.py
+++ owobot/uwubot.py
@@ -11,21 +11,29 @@ class UwuBot(BotBase):
         config = Config(config_path)
 
         intents = discord.Intents().all()
-        allowed_mentions = discord.AllowedMentions(users=True, everyone=False, roles=False, replied_user=False)
+        allowed_mentions = discord.AllowedMentions(
+            users=True, everyone=False, roles=False, replied_user=False)
         super().__init__(command_prefix=config.command_prefix, description=config.desc, intents=intents,
                          allowed_mentions=allowed_mentions)
         self.config = config
 
         # Load cogs
-        src_dir, _ = path.split(path.realpath(__file__))
-        cogs_path = path.join(src_dir, "../cogs")
-        cogs = os.listdir(cogs_path)
+        cogs = [os.path.splitext(f)[0] for f in os.listdir("cogs")
+                if f.endswith(".py")]
 
         # TODO: temporary fix as cogs not working
-        blocked_cogs = ["gallery.py", "msg_writer.py", "stats.py"]
+        blocked_cogs = ["gallery", "msg_writer", "stats", "quotes"]
         cogs = list(filter(lambda x: x not in blocked_cogs, cogs))
 
         for cog in cogs:
-            importlib.import_module(cog)
-        
+            module = __import__("cogs.%s" % cog, globals(),
+                                locals(), fromlist=["*"])
+
+            for item_name in dir(module):
+                try:
+                    new_cog = getattr(module, item_name)()
+                    self.add(new_cog)
+                except:
+                    pass
+
         # TODO: dynamically add cogs to bot
