27e8f713017b4fbe0c310a0074e4531b32bcd618
Simon Huang <simon.huang@tum.de>
2022-03-26 19:17:42 +0100

    remove redundant attributes

--- owobot/uwubot.py
+++ owobot/uwubot.py
@@ -7,7 +7,7 @@ from discord.ext.commands import Bot as BotBase
 
 
 class UwuBot(BotBase):
-    def __init__(self, config_path, command_prefix, **options):
+    def __init__(self, config_path):
         config = Config(config_path)
 
         intents = discord.Intents().all()
@@ -20,6 +20,8 @@ class UwuBot(BotBase):
         src_dir, _ = path.split(path.realpath(__file__))
         cogs_path = path.join(src_dir, "../cogs")
         cogs = os.listdir(cogs_path)
+
+        # TODO: temporary fix as cogs not working
         blocked_cogs = ["gallery.py", "msg_writer.py", "stats.py"]
         cogs = list(filter(lambda x: x not in blocked_cogs, cogs))
 
