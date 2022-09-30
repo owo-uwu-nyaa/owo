b3a08df70158080972cd1015e3ba4e473bae9c3c
Max Lang <17551908+just-max@users.noreply.github.com>
2022-09-30 04:56:36 +0200

    sus: better handle builtin emojis

--- owobot/cogs/sus.py
+++ owobot/cogs/sus.py
@@ -3,15 +3,15 @@ import re
 import functools
 import time
 from datetime import timedelta
-from pathlib import Path
+import itertools as it
+import more_itertools as mit
 
 import discord
 from discord.ext import commands
-import emoji as emojilib
 
-from owobot import misc
 from owobot.owobot import OwOBot
-from owobot.misc import common, suslib, owolib
+from owobot.misc import common, suslib, owolib, discord_emoji
+
 
 from typing import Optional
 
@@ -28,58 +28,70 @@ class Sus(commands.Cog):
     @classmethod
     @functools.lru_cache(maxsize=None)  # equivalent to functools.cache in python >= 3.10
     def _emoji_names_dictionary(cls):
-        data_file = Path(misc.__file__).parent / "emoji_list"
-        emoji_names = []
-        with data_file.open("r") as df:
-            for line in filter(lambda sl: sl, (l.strip() for l in df)):
-                emoji_names.append(line.split(" ")[0].lower())
+        log.info("start building emoji name dictionary")
+        start = time.process_time_ns()
 
-        log.info(f"loaded {len(emoji_names)} emoji names from disk")
+        result = suslib.build_dictionary(
+            name.lower()
+            for name, emoji in discord_emoji.EMOJIS_BY_NAME.items()
+            if not isinstance(emoji, discord_emoji.DiversityChild)
+        )
 
-        return suslib.build_dictionary(emoji_names)
+        end = time.process_time_ns()
+        log.info("finished building emoji dictionary in %s", timedelta(microseconds=(end - start) // 1000))
+
+        return result
 
     @classmethod
     def build_emoji_name_dictionary(cls):
-        log.info("start building emoji name dictionary")
-        start = time.process_time_ns()
         cls._emoji_names_dictionary()
-        end = time.process_time_ns()
-        log.info("finished building emoji dictionary in %s", timedelta(microseconds=(end - start) // 1000))
 
     @commands.hybrid_command(
         brief="find the shortest way to describe an emoji when inputting it on this server :postbox:"
     )
     @common.long_running_command
-    async def sus(self, ctx: commands.Context, emoji: str, nitro: Optional[bool] = None, include_builtin=True):
-        if emoji in emojilib.EMOJI_DATA:
-            emoji_repr = emoji
-            # TODO?: check all aliases, also not fully correct
-            emoji_name = Sus._EMOJI_RE.fullmatch(emojilib.EMOJI_DATA[emoji]["en"])["name"]
+    async def sus(
+            self,
+            ctx: commands.Context,
+            emoji: str,
+            nitro: Optional[bool] = None,
+            include_builtin=True
+    ):
+        if emoji in discord_emoji.EMOJIS_BY_SURROGATES:
+            emoji_str = builtin_emoji = discord_emoji.EMOJIS_BY_SURROGATES[emoji]
+            candidates = builtin_emoji.names
             nitro = False if nitro is None else nitro
         else:
-            emoji_repr = d_emoji = discord.PartialEmoji.from_str(emoji)
-            emoji_name = d_emoji.name
-            nitro = d_emoji.animated if nitro is None else nitro
-        suss = tuple(map(
-            lambda su: f"`{su}`",
+            emoji_str = server_emoji = discord.PartialEmoji.from_str(emoji)
+            candidates = (server_emoji.name, )
+            nitro = server_emoji.animated if nitro is None else nitro
+
+        server_dictionary = suslib.build_dictionary(
+            emoji.name.lower() for emoji in ctx.guild.emojis if nitro or not emoji.animated
+        )
+
+        suss_per_candidate = (
             suslib.shortest_unique_substring(
-                emoji_name.lower(),
+                candidate.lower(),
                 *(Sus._emoji_names_dictionary(),) if include_builtin else (),
-                suslib.build_dictionary(
-                    emoji.name.lower()
-                    for emoji in ctx.guild.emojis
-                    if nitro or not emoji.animated)
-            )
-        ))
+                server_dictionary)
+            for candidate in candidates
+        )
+
+        suss = tuple(map(lambda sus: f"`{sus}`", mit.unique_everseen(it.chain.from_iterable(common.minima(
+            suss_per_candidate,
+            # put non-empty result tuples first, then sort by the length of the shortest unique substring(s)
+            key=lambda suss0: (0, suss0[0]) if suss0 else (1, None)
+        )))))
 
         await ctx.send(
-            f"shortest way to describe {emoji_repr} is "
+            f"shortest way to describe {emoji_str} is "
             + ", ".join(suss[:-1]) + (" or " if len(suss) > 1 else "") + suss[-1]
             if suss
-            else f"{owolib.get_random_sorry()}: {owolib.owofy('no unique way to describe')} {emoji_repr}"
+            else f"{owolib.get_random_sorry()}: {owolib.owofy('no unique way to describe')} {emoji_str}"
         )
 
 
-def setup(bot):
+async def setup(bot):
     Sus.build_emoji_name_dictionary()
-    return bot.add_cog(Sus(bot))
+    await bot.add_cog(Sus(bot))
---
+++ owobot/misc/discord_emoji.py
@@ -0,0 +1,147 @@
+from dataclasses import dataclass
+from decimal import Decimal
+import pkgutil
+import json
+from types import MappingProxyType
+
+from typing import Tuple, List, Iterable, TypeVar, Callable, Mapping
+
+
+_discord_emoji_dataclass = dataclass(frozen=True)
+
+
+T = TypeVar("T")
+
+
+def _identity(x: T) -> T:
+    return x
+
+
+@_discord_emoji_dataclass
+class Emoji:
+    names: Tuple[str]
+    surrogates: str
+    unicode_version: Decimal
+    category: "EmojiCategory"
+
+    def __iter__(self):
+        yield self
+
+    def __str__(self):
+        return self.surrogates
+
+
+@_discord_emoji_dataclass
+class DiversityParent(Emoji):
+    diversity_children: Tuple["DiversityChild"]
+
+    def __iter__(self):
+        yield from super().__iter__()
+        for child in self.diversity_children:
+            yield from child
+
+
+@_discord_emoji_dataclass
+class DiversityChild(Emoji):
+    diversity: Tuple[str]
+    diversity_parent: "DiversityParent"
+
+
+# doesn't actually exist
+@_discord_emoji_dataclass
+class DiversityInnerNode(DiversityParent, DiversityChild):
+    pass
+
+
+@_discord_emoji_dataclass
+class EmojiCategory:
+    name: str
+    emojis: Tuple[Emoji]
+
+    def __iter__(self):
+        for emoji in self.emojis:
+            yield from emoji
+
+
+def _build(data, builder, class_: Callable[[dict], T]) -> T:
+    return class_(**{key: mk if data_key is None else mk(data[data_key]) for key, (data_key, mk) in builder.items()})
+
+
+def _load_diversity(data: List[str]) -> Tuple[str]:
+    return tuple(chr(int(d, base=16)) for d in data)
+
+
+def _load_emoji(data: dict) -> Emoji:
+    builder = dict(
+        names=("names", tuple),
+        surrogates=("surrogates", _identity),
+        unicode_version=("unicodeVersion", _identity),
+        category=(None, None)
+    )
+    emoji_class: type(Emoji) = Emoji
+
+    if data.get("hasDiversity", False):
+        builder.update(diversity_children=("diversityChildren", _load_emojis))
+        emoji_class = DiversityParent
+
+    if data.get("hasDiversityParent", False):
+        builder.update(
+            diversity=("diversity", _load_diversity),
+            diversity_parent=(None, None)
+        )
+        emoji_class = DiversityChild
+
+    if data.get("hasDiversity", False) and data.get("hasDiversityParent", False):
+        emoji_class = DiversityInnerNode
+
+    result: Emoji = _build(data, builder, emoji_class)
+
+    if isinstance(result, DiversityParent):
+        for child in result.diversity_children:
+            object.__setattr__(child, "diversity_parent", result)  # bypass frozen
+
+    return result
+
+
+def _load_emojis(data: Iterable[dict]) -> Tuple[Emoji]:
+    return tuple(map(_load_emoji, data))
+
+
+def _load_category(data: dict) -> EmojiCategory:
+    builder = dict(
+        name=(0, _identity),
+        emojis=(1, _load_emojis)
+    )
+
+    result = _build(data, builder, EmojiCategory)
+
+    for emoji in result:
+        object.__setattr__(emoji, "category", result)  # bypass frozen
+
+    return result
+
+
+def _load_categories(data: dict) -> Tuple[EmojiCategory]:
+    return tuple(map(_load_category, data.items()))
+
+
+CATEGORIES = _load_categories(
+    json.loads(
+        pkgutil.get_data(__name__, "data/discord_emoji.json").decode("utf-8")
+    )
+)
+
+
+def iter_emojis(categories=CATEGORIES):
+    for category in categories:
+        yield from category
+
+
+EMOJIS_BY_NAME: Mapping[str, Emoji] = MappingProxyType(
+    {name: emoji for emoji in iter_emojis() for name in emoji.names}
+)
+
+
+EMOJIS_BY_SURROGATES: Mapping[str, Emoji] = MappingProxyType(
+    {emoji.surrogates: emoji for emoji in iter_emojis()}
+)
